ed72a36f4345b289b67861928e198b0b643d2674
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2025-05-25 21:09:03 -0300

    Update Temporizador.java

--- src/jogo/Temporizador.java
+++ src/jogo/Temporizador.java
@@ -1,1 +1,28 @@
+package jogo;
 
+public class Temporizador {
+
+    private long inicio;
+    private long fim;
+
+    public void iniciar() {
+        inicio = System.currentTimeMillis();
+        fim = 0; 
+    }
+
+    public void finalizar() {
+        fim = System.currentTimeMillis();
+    }
+
+    public long getDuracaoEmSegundos() {
+        long tempoFinal = (fim > 0) ? fim : System.currentTimeMillis();
+        return (tempoFinal - inicio) / 1000;
+    }
+
+    public String formatarTempo() {
+        long segundos = getDuracaoEmSegundos();
+        long minutos = segundos / 60;
+        long restoSegundos = segundos % 60;
+        return String.format("%02d:%02d", minutos, restoSegundos);
+    }
+}
