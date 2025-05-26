91eedb16ee1adbe171338cd0e2679155b7791cc3
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2025-05-25 21:08:01 -0300

    Update ExibidorStatus.java

--- src/jogo/ExibidorStatus.java
+++ src/jogo/ExibidorStatus.java
@@ -1,1 +1,15 @@
+package jogo;
 
+import personagem.Aventureiro;
+
+public class ExibidorStatus {
+
+    public static void mostrarStatus(Aventureiro aventureiro, Temporizador temporizador) {
+        System.out.println("---------------------------------");
+        System.out.println("Nome: " + aventureiro.getNome());
+        System.out.println("Vida: " + aventureiro.getVida());
+        System.out.println("Posição: (" + aventureiro.getPosX() + ", " + aventureiro.getPosY() + ")");
+        System.out.println("Tempo: " + temporizador.formatarTempo());
+        System.out.println("---------------------------------");
+    }
+}
